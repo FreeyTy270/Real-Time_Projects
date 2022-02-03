e106c7ba05b7e6bff5f0a88bfb7a70654cac0c39
FreeTryy <freemanty27@gmail.com>
2022-02-03 15:15:23 -0500

    Upload all current projects to repo

---
+++ TKF_Project1_Timers/Core/Inc/clock.h
@@ -0,0 +1,13 @@
+/*
+ * clock.h
+ *
+ *  Created on: Feb 1, 2022
+ *      Author: Ty Freeman
+ */
+
+#ifndef INC_CLOCK_H_
+#define INC_CLOCK_H_
+
+void clock_init(void);
+
+#endif /* INC_CLOCK_H_ */
