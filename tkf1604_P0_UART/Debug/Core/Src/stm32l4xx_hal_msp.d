5ece9e82136a0b844d5f7212cebb2f2ccfa0a2e9
FreeTryy <freemanty27@gmail.com>
2022-08-26 15:43:10 -0400

    Create new project 0 remove old one
    Blinky project made to test env and serial.

--- tkf1604_P0_UART/Debug/Core/Src/stm32l4xx_hal_msp.d
+++
@@ -1,4 +0,0 @@
-Core/Src/stm32l4xx_hal_msp.o: ../Core/Src/stm32l4xx_hal_msp.c \
- ../Core/Inc/main.h
-
-../Core/Inc/main.h:
--- tkf1604_P0_UART/Debug/Core/Src/stm32l4xx_it.d
+++
@@ -1,6 +0,0 @@
-Core/Src/stm32l4xx_it.o: ../Core/Src/stm32l4xx_it.c ../Core/Inc/main.h \
- ../Core/Inc/stm32l4xx_it.h
-
-../Core/Inc/main.h:
-
-../Core/Inc/stm32l4xx_it.h:
