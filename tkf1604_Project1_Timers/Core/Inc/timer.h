aea2b302a4e1cc1cde36b4ec02c21a9a45a8d019
FreeTryy <freemanty27@gmail.com>
2022-09-02 20:05:24 -0400

    Created Project 1
    Created project and then brought it up to the point I had left it previously.

---
+++ tkf1604_Project1_Timers/Core/Inc/timer.h
@@ -0,0 +1,14 @@
+/*
+ * timer.h
+ *
+ *  Created on: Aug 31, 2022
+ *      Author: Ty Freeman
+ */
+
+#ifndef INC_TIMER_H_
+#define INC_TIMER_H_
+
+static void TIM_INIT(void);
+
+
+#endif /* INC_TIMER_H_ */
